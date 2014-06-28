66dfc397ccb9561103b64de4c9b6ba5f68be354d
Matthew Nienow <rockopera8@gmail.com>
2014-06-28 00:09:02 -0500

    More work on checkboxes

--- checkbox/checkbox.js
+++ checkbox/checkbox.js
@@ -1,15 +1,15 @@
 !function($) {
   
-  $.fn.lCheckbox = function() {
-    this.addClass('large-checkbox-field');
-    this.append('<input class="large-checkbox" id="large-checkbox" type="checkbox"/>');
-    this.append('<label class="large-checkbox-label" for="large-checkbox"></label>');
-  };
-  
-  $.fn.lCheckbox2 = function(options) {
+  $.fn.lCheckbox = function(options) {
+		if (!options)
+			options = {};
     this.addClass('setting-b');
+		if (!options.img)
+			options.img = 'http://cdn2.iconfinder.com/data/icons/flat-ui-icons-24-px/24/checkmark-24-512.png';
     this.append('<div class="setting-img centered" style="background-image: url(' + options.img + '); background-repeat: no-repeat; background-position: center"></div>');
-    this.append('<div>' + options.text + '</div>');
+    if (options.text) {
+			this.append('<div>' + options.text + '</div>');
+		}
     this.click(function() {
       $(this).toggleClass('true');
     });
